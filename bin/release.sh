f6bb6307d3a2315d9582c87ea05297ee933b484c
Ana Paula Gomes <apgomes88@gmail.com>
2019-12-31 13:07:06 -0300

    Mostra PATH depois da configuração

--- bin/release.sh
+++ bin/release.sh
@@ -4,10 +4,11 @@ set -eo pipefail
 
 echo "Configuring Java path..."
 export PATH=/app/.apt/usr/lib/jvm/default-java/bin:$PATH
+echo $PATH
 
 echo "Installing Tika..."
 wget -P scraper/tika-server/tika-server-1.23.jar https://www.apache.org/dyn/closer.cgi/tika/tika-server-1.23.jar
 export TIKA_SERVER_JAR=$(pwd)/scraper/tika-server/tika-server-1.23.jar
-echo TIKA_SERVER_JAR=$TIKA_SERVER_JAR
+echo $TIKA_SERVER_JAR
 
 echo "Done!"
