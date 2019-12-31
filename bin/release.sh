1a13e9a2af5c8957aced67b4d23408e4112fec8e
Ana Paula Gomes <apgomes88@gmail.com>
2019-12-31 12:47:05 -0300

    Adiciona o Java ao PATH

--- bin/release.sh
+++ bin/release.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+echo "Configuring Java path..."
+export PATH=/app/.apt/usr/lib/jvm/default-java/bin:$PATH
+
 echo "Installing Tika..."
 wget -P scraper/tika-server/tika-server-1.23.jar https://www.apache.org/dyn/closer.cgi/tika/tika-server-1.23.jar
 export TIKA_SERVER_JAR=$(pwd)/scraper/tika-server/tika-server-1.23.jar
